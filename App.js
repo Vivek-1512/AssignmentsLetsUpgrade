b5853780bf7fbcd6764e5e6244261174add7a4f7
Vivek-1512 <75220316+Vivek-1512@users.noreply.github.com>
2021-07-14 17:48:22 +0530

    Day1 Assignment

---
+++ App.js
@@ -0,0 +1,13 @@
+import './style.css';
+import Button from './Botton';
+
+function App() {
+  return (
+    <div className="hello">
+       <h1 style={{textAlign: 'center', border: '2px solid black',background:'pink'}}>Hello World</h1>
+      <Button />
+    </div>
+  );
+}
+
+export default App;
---
+++ Botton.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Button(){
+    return(
+        <div>
+        <button>Button 1</button>
+        </div>
+    );
+}
+
+export default Button;
